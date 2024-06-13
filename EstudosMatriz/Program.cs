ad2e72bf5256f66389ba419c522170e44343e30e
CaioAdania <caioadania@gmail.com>
2024-06-13 11:23:19 -0300

    matrizes, adicionando mais complexidade

--- EstudosMatriz/Program.cs
+++ EstudosMatriz/Program.cs
@@ -4,16 +4,40 @@
     {
         static void Main(string[] args)
         {
-            double[,] mat = new double[2, 3];/*A "," indica a inicialização de um arranja bidimensional com coluna e linha
-                                             e na instancia podemos dizer quantos linhas queremos no caso 2 e quantas colunas no caso 3*/
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(mat.Length); //quantidade de "slots"
+            int[,] mat = new int[n, n];
 
-            Console.WriteLine(mat.Rank); //quantidade de linhas
+            for(int i = 0; i < n; i++)// para cada uma das linhas **
+            {
+                string[] values = Console.ReadLine().Split(" "); //aqui estamos lendo a linha inteira e recortando por "ESPAÇO" e guardando dentro do vetor values
 
-            Console.WriteLine(mat.GetLength(0)); //a dimensão 0 da matriz, ela tem dimensão 2 que é a quantidade de linhas 
+                for(int j =0; j < n; j++) //responsavel por percorrer as colunas *
+                {
+                    mat[i, j] = int.Parse(values[j]); //a coluna 0, guarda o values[0], pois ainda não houve incremento, guardando tudo na coluna 0
+                    // é usado o int.Parse pois quando fazemos o vetor, ele é uma string e o int.Parse, converte para int para ser guardado na matriz
+                }
+            }
 
-            Console.WriteLine(mat.GetLength(1)); //a dimensão 1 da matriz, ela tem 3 dimensões que é a quantidade de colunas
+            Console.WriteLine("Main Diagonal: ");
+            for (int i =0; i<n; i++) //for para mudar o numero da matriz e percorrer toda diagonal
+            {
+                Console.Write(mat[i, i] + " "); //a matriz recebe i,i, pois a diagonal sempre é 0,0 1,1 2,2
+            }
+            Console.WriteLine();
+
+            int count = 0; //variavel iniciada em 0
+            for(int i =0; i<n;i++) //percorrer as linhas
+            {
+                for (int j =0; j<n; j++) //percorrer as colunas
+                {
+                    if (mat[i,j] < 0) //verificação de numero <0
+                    {
+                        count++; //cada vez que ele encontrar um numero < 0, ele faz o incremento
+                    }
+                }
+            }
+            Console.WriteLine("Negative Numbers: " + count);
         }
     }
 }
--- EstudosMatriz/obj/Debug/net8.0/EstudosMatriz.AssemblyInfo.cs
+++ EstudosMatriz/obj/Debug/net8.0/EstudosMatriz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstudosMatriz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f92784c3802c3a302e50717c908f5ee65410206")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstudosMatriz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstudosMatriz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
